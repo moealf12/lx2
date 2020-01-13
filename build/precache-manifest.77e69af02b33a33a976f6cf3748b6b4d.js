443e6c8a5674db9569681d7261f7c983bdf5a881
Mohammed Alfisal <mohammedalfisal@Mohammeds-MacBook-Pro.local>
2020-01-14 01:52:48 +0300

    Head Fix xD 3

--- build/precache-manifest.77e69af02b33a33a976f6cf3748b6b4d.js
+++ build/precache-manifest.77e69af02b33a33a976f6cf3748b6b4d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "788c3f25dc84f2d8544f1bb1c728404d",
+    "revision": "a49c7675437a02aa5d09580b459ceb1e",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "3525c0730395a8dbbf1d",
-    "url": "/static/css/main.bb3fb741.chunk.css"
+    "revision": "5ec7b5554898a76b605b",
+    "url": "/static/css/main.ca532da0.chunk.css"
   },
   {
     "revision": "98e101665e089e2e1553",
     "url": "/static/js/2.b7c032c5.chunk.js"
   },
   {
-    "revision": "3525c0730395a8dbbf1d",
-    "url": "/static/js/main.167f3ea7.chunk.js"
+    "revision": "5ec7b5554898a76b605b",
+    "url": "/static/js/main.f3b3bddd.chunk.js"
   },
   {
     "revision": "596433eb55e8ea15a620",
